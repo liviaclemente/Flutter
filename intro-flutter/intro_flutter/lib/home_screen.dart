4e793b454f37e2190e103b68f31b115ca1f4207c
Lívia <livia.clemente@aluno.senai.br>
2025-08-22 13:47:30 -0300

    Upload de Pasta

---
+++ intro-flutter/intro_flutter/lib/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:intro_flutter/components/custom_card.dart';
+import 'package:intro_flutter/exemples/value_notifier_counter_screen.dart';
+import 'package:intro_flutter/pages/counter_screen_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
+      ),
+      body: Padding(padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                children: [CustomCard(
+                  title: 'Counter Screen', 
+                  description: 'Este é uma tela de contador interativo.', 
+                  icon: Icons.add_circle_outline, 
+                  destination: CounterScreenValueNotifier()
+                  )
+                ],
+              )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
